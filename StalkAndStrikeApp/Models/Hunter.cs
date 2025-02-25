cfdf079fad2f81420c75b005457f6bd7ec236f2d
kirilexe <kirilkokokisiov@gmail.com>
2025-02-25 17:35:29 +0200

    Make better design

--- StalkAndStrikeApp/Models/Hunter.cs
+++ StalkAndStrikeApp/Models/Hunter.cs
@@ -1,12 +1,18 @@
 ﻿namespace StalkAndStrikeApp.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Hunter
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "License Number is required")]
+        public string LicenseNumber { get; set; } = string.Empty;  // Ensure it's not NULL
+
         public string? Name { get; set; }
-        public string? LicenseNumber { get; set; }
         public int SquadId { get; set; }
         public Squad? Squad { get; set; }
         public ICollection<Dog> Dogs { get; set; } = new List<Dog>();
     }
+
 }
